d96c4bfbe610bf20de0633d0d271d11f61ea220a
ArtKiwi <mr.vlmars@mail.ru>
2017-05-06 19:21:57 +0300

    andrey correcter

--- loginsys/forms.py
+++ loginsys/forms.py
@@ -16,11 +16,11 @@ class MyAuthenticationForm(AuthenticationForm):
     def confirm_login_allowed(self, user):
         if not user.is_active:
             raise forms.ValidationError(
-                _("This account is inactive."),
+                "This account is inactive.",
                 code='inactive',
             )
         if user.username.startswith('b'):
             raise forms.ValidationError(
-                _("Sorry, accounts starting with 'b' aren't welcome here."),
+                "Sorry, accounts starting with 'b' aren't welcome here.",
                 code='no_b_users',
             )
